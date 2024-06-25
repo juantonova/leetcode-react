7ef3b89bde057e1df52b97a95bc27a7d2bdf6614
antonova.u <antonova.u@ati.su>
2024-06-25 22:51:23 +0300

    Revert "leetcode-react: task pages added"
    This reverts commit aed8945b34afea6e226ea12100a001a6faf72d3b.

--- src/models/common.ts
+++
@@ -1,8 +0,0 @@
-export const enum AppRoutes {
-    HOME = '/',
-    TASKS = '/tasks',
-    TASK = '/tasks/:id',
-    TASK_EDIT = '/tasks/:id/edit',
-    USERS = '/users',
-    USER = '/users/:id',
-}
--- src/models/users.ts
+++
@@ -1,8 +0,0 @@
-export type User = {
-    id: number
-    role: string
-    name: string
-    permissions: string[]
-    rating?: number
-  }
-  
