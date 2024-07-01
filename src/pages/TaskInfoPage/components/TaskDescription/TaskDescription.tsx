2ff9bb675e3c5524a73199f1c267e8783082676f
antonova.u <antonova.u@ati.su>
2024-07-01 20:54:34 +0300

    add map for task description

--- src/pages/TaskInfoPage/components/TaskDescription/TaskDescription.tsx
+++ src/pages/TaskInfoPage/components/TaskDescription/TaskDescription.tsx
@@ -1,39 +1,42 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import React from "react";
 import { Descriptions } from "antd";
 
 import Tags from "../../../../components/Tags";
-import { Task } from "../../../../models/tasks";
+import { Tag, Task } from "../../../../models/tasks";
 
 type Props = {
   task: Task;
 };
 
+type TaskInfoKeys = keyof Omit<Task, "id">
+
+const labelDescriptions: { [key in TaskInfoKeys]: string } = {
+  description: "Описание",
+  incoming_example: "Пример ввода",
+  outgoing_example: "Пример вывода",
+  tags: "Теги",
+  category: "Категория",
+  score: "Сложность",
+  title: "Название",
+  additional_info: "Дополнительная информация",
+}
+
+
 const TaskDescription: React.FC<Props> = ({ task }) => {
   const {
-    title,
-    category,
-    description,
-    score,
-    incoming_example,
-    outgoing_example,
-    tags,
+    id, ...restTaskInfo
   } = task || {};
+
+   const descriptions = Object.entries(restTaskInfo).map(([key, value], index) => {
+   const description = key === "tags" ? <Tags tags={value as Tag[]} /> : value
+   const label = key in labelDescriptions ? labelDescriptions[key as TaskInfoKeys] : ""
+    return <Descriptions.Item key={index} label={label}>{description}</Descriptions.Item>
+   })
 
   return (
     <Descriptions title="Информация о задаче" bordered>
-      <Descriptions.Item label="Название">{title}</Descriptions.Item>
-      <Descriptions.Item label="Категория">{category}</Descriptions.Item>
-      <Descriptions.Item label="Входящие данные">
-        {incoming_example}
-      </Descriptions.Item>
-      <Descriptions.Item label="Теги">
-        <Tags tags={tags} />
-      </Descriptions.Item>
-      <Descriptions.Item label="Сложность">{score}</Descriptions.Item>
-      <Descriptions.Item label="Исходящие данные">
-        {outgoing_example}
-      </Descriptions.Item>
-      <Descriptions.Item label="Описание">{description}</Descriptions.Item>
+      {descriptions}
     </Descriptions>
   );
 };
